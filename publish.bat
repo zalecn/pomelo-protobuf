667bd47ec545da67440e6ce3759d2ea970d0c43f
zalecn <zhumingjian1229@163.com>
2017-03-30 20:23:11 +0800

    support map type

---
+++ publish.bat
@@ -0,0 +1,5 @@
+CALL npm config set registry http://registry.npmjs.org
+CALL npm publish .
+CALL npm config set registry https://registry.npm.taobao.org
+
+pause
---
+++ unpublish.bat
@@ -0,0 +1,5 @@
+CALL npm config set registry http://registry.npmjs.org
+CALL npm unpublish .
+CALL npm config set registry https://registry.npm.taobao.org
+
+pause
